0503d3c4769366b3dc2cbb9c208bdc64b7718423
Exodia <truongminhhien142@gmail.com>
2017-12-06 16:10:59 +0700

    update mongo connection

--- routes/mongoose/mongoose.js
+++ routes/mongoose/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 // mongoose.connect('mongodb://admin@accessfree.tk:27017/mydbs', { useMongoClient: true });
-mongoose.connect('mongodb://admin@db:27017/mydbs', { useMongoClient: true });
+mongoose.connect('mongodb://admin:admin@ds129906.mlab.com:29906/accessfree', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 exports.Cat = mongoose.model('Cat', { name: String });
